25f2959a8c0a8605a643443d3aa4f54a3ea4bbcf
CrazyChickenDev <dannychukz15@gmail.com>
2020-04-30 23:36:57 +0100

    dev-test edited

--- dev-test.js
+++ dev-test.js
@@ -1,5 +1,4 @@
 const Block = require('./block');
 
-const block = new Block('foo', 'bar', 'zoo', 'baz');
-console.log(block.toString());
-console.log(Block.genesis().toString());
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+console.log(fooBlock.toString());
