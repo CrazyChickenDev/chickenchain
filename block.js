7a5a2c33f02b3d5acd2453a4ede15a017e6f6d14
CrazyChickenDev <dannychukz15@gmail.com>
2020-04-30 23:34:30 +0100

    added mineBlock

--- block.js
+++ block.js
@@ -17,6 +17,14 @@ class Block {
     static genesis() {
         return new this('Genesis time', '-----', 'f1r57-h45h', []);
     }
+
+    static mineBlock(lastBlock, data) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        const hash = 'todo-hash';
+
+        return new this(timestamp, lastHash, hash, data);
+    }
 }
 
 module.exports = Block;
