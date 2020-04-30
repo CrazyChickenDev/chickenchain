9f203674a65aa84f9024baefb5c349c3015c0f06
CrazyChickenDev <dannychukz15@gmail.com>
2020-04-30 23:02:57 +0100

    modified block.js

--- block.js
+++ block.js
@@ -1,3 +1,16 @@
 class Block {
-    ..constructor(timestamp, lastHash) {}
+    constructor(timestamp, lastHash, hash, data) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+    toString() {
+        return `Block -
+      Timestamp: ${this.timestamp} 
+      Last Hash: ${this.lastHash}
+      Hash     : ${this.hash}
+      Data     : ${this.data}`;
+    }
 }
